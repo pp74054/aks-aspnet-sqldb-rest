639ba8ffe56ddbe0e881f43c6cc9d067864a1e2a
pp74054 <p123rasad@gmail.com>
2019-11-14 16:14:59 -0800

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,7 @@ namespace ClaimsApi
             services.AddControllers();
             // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2); ID11092019.o
 
-            /**
+            
             // Register the Swagger generator, defining 1 or more Swagger documents
             services.AddSwaggerGen(c =>
             {
@@ -69,7 +69,7 @@ namespace ClaimsApi
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
             });
-            **/
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -85,7 +85,7 @@ namespace ClaimsApi
                 app.UseHsts();
             }
 
-            /**
+            
             // Enable middleware to serve generated Swagger as a JSON end-point
             app.UseSwagger();
             // Enable middleware to serve Swagger-ui, specifying Swagger JSON end-point.
@@ -93,7 +93,7 @@ namespace ClaimsApi
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Claims API");
             });
-            **/
+            
              
             // app.UseHttpsRedirection();
             // Add MVC middleware
